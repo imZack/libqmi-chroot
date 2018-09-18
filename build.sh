d75acc05fdf45a15a075dd00ebe92f18b7c3d510
Zack YL Shih <zackyl.shih@moxa.com>
2018-09-17 23:36:13 -0700

    Fix wrong qemu

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ export DEB_VERSION=sid
 
 docker run --rm --privileged multiarch/qemu-user-static:register
 
-curl -LO https://github.com/multiarch/qemu-user-static/releases/download/v2.12.0-1/qemu-armeb-static
+curl -LO https://github.com/multiarch/qemu-user-static/releases/download/v2.12.0-1/qemu-arm-static
 
 if [ "$ARCH" = "armv7" ]; then
   docker run -i --rm -v "$(pwd)/output:/var/jail" \
     -v "$(pwd)/create-chroot.sh:/create-chroot.sh" \
-    -v "$(pwd)/qemu-armeb-static:/usr/bin/qemu-armeb-static" \
+    -v "$(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static" \
     arm32v7/debian:$DEB_VERSION bash /create-chroot.sh
 fi
 
