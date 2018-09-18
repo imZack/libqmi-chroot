010a267912c616f63a5223df48d9408a05d62e81
Zack YL Shih <zackyl.shih@moxa.com>
2018-09-17 23:55:26 -0700

    Test

--- build.sh
+++ build.sh
@@ -7,12 +7,20 @@ docker run --rm --privileged multiarch/qemu-user-static:register
 
 curl -LO https://github.com/multiarch/qemu-user-static/releases/download/v2.12.0-1/qemu-arm-static
 
+file qemu-arm-static
+
 if [ "$ARCH" = "armv7" ]; then
   docker run -i --rm  --privileged \
     -v "$(pwd)/output:/var/jail" \
     -v "$(pwd)/create-chroot.sh:/create-chroot.sh" \
     -v "$(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static" \
     arm32v7/debian:$DEB_VERSION bash /create-chroot.sh
+
+  docker run -i --rm  --privileged \
+    -v "$(pwd)/output:/var/jail" \
+    -v "$(pwd)/create-chroot.sh:/create-chroot.sh" \
+    -v "$(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static" \
+    multiarch/debian-debootstrap:armhf-sid bash /create-chroot.sh
 fi
 
 if [ "$ARCH" = "amd64" ]; then
